17f4ad9156420ff057677c7ca13298e13793da65
viktorrrrr <viktor.cvetanovic.4421@metropolitan.ac.rs>
2021-11-19 09:51:41 +0100

    changed install name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pip._internal.req import parse_requirements
 install_reqs = [str(req.requirement) for req in parse_requirements('requirements.txt', session='hack')]
 
 setup(
-    name='zoom',
+    name='zoomstarter',
     version='1.0',
     packages=["zoom"],
     package_data={'': ['data/config.json']},
