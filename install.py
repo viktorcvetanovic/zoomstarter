257ada08a5e9c97ba11f4bab951cf706678a0865
Viktorrrrr <viktorcvetanovic@gmail.com>
2022-03-01 21:15:09 +0100

    fix path

--- install.py
+++ install.py
@@ -5,6 +5,6 @@ os.system('cd '+cwd)
 os.system('python setup.py install')
 os.system('echo END INSTALLATION')
 os.system("pyinstaller --onefile --icon=ico.ico runner.py")
-desktop =  os.environ['USERPROFILE']+"\\Desktop"
+desktop =  desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
 shutil.move("dist\\runner.exe", desktop+"\\zoomstarter.exe")
 os.system('echo kraj')
