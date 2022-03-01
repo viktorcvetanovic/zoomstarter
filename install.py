962790120aa695c9ee19870d39670adcc2b747c8
Viktorrrrr <viktorcvetanovic@gmail.com>
2022-03-01 21:07:37 +0100

    changed path

--- install.py
+++ install.py
@@ -5,6 +5,6 @@ os.system('cd '+cwd)
 os.system('python setup.py install')
 os.system('echo END INSTALLATION')
 os.system("pyinstaller --onefile --icon=ico.ico runner.py")
-desktop =  os.environ['USERPROFILE']+"\\OneDrive\\Desktop"
+desktop =  os.environ['USERPROFILE']+"\\Desktop"
 shutil.move("dist\\runner.exe", desktop+"\\zoomstarter.exe")
 os.system('echo kraj')
